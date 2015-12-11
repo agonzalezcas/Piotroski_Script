b152df527d22d45c4311bcfe93b0fbea093b5e20
axlevisu <axlevisu@gmail.com>
2015-12-11 13:25:06 +0530

    bug fixes

--- alt_script.py
+++ alt_script.py
@@ -25,9 +25,7 @@ set_list_indexing = ['n','h','p','c','f']
 set_list_dict = {set_list[i]:set_list_indexing[i] for i in xrange(5)}
 for i in xrange(len(useful_info[1])):
 	useful_info[1][i] = set_list_dict[useful_info[0][i]]+useful_info[1][i]
-	print useful_info[1][i]
 idx= {useful_info[1][i] : i for i in xrange(len(useful_info[1]))}
-print idx
 # Delete Unnecessary data
 del data[0:5]
 #convert to float(to 0 in case of NA and no data)
@@ -39,15 +37,13 @@ for i in xrange(0,len(data)):
 			data[i][j] = 0.0
 # Calculate F_SCORE
 f_score = []
-# for x in idx:
-# 	print data[0][idx[x]] 
 # Remove companies without useful data
 companies_for_removal =[]
 for company in data:
 	try:
 		score =0
 		score+=1 if ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) >0 else 0 #ROA
-		score+=1 if ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['cTotal assets']] ) > ( (company[idx['pTotal income']]-company[idx['pTotal expenses']]-company[idx['pExtra-ordinary income']]+company[idx['pExtra-ordinary expenses']])/company[idx['hTotal assets']] )  else 0 #D_ROA
+		score+=1 if ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) < ( (company[idx['pTotal income']]-company[idx['pTotal expenses']]-company[idx['pExtra-ordinary income']]+company[idx['pExtra-ordinary expenses']])/company[idx['hTotal assets']] )  else 0 #D_ROA
 		score+=1 if ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] ) >0 else 0 #CFO
 		score+=1 if  ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) > ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] )  else 0 #ACCRUAL
 		score+=1 if ( company[idx['cCurrent assets']]/company[idx['cCurrent liabilities']] ) >  ( company[idx['pCurrent assets']]/company[idx['pCurrent liabilities']] ) else 0 #D_LIQUID
@@ -58,7 +54,7 @@ for company in data:
 		except:
 			score+=1 if ( (company[idx['cIncome from financial services']]-company[idx['cCost of goods sold']])/company[idx['cIncome from financial services']] ) >( (company[idx['pIncome from financial services']]-company[idx['pCost of goods sold']])/company[idx['pIncome from financial services']] ) else 0 
 		try:#D_TURNOVER
-			score+=1 if ( company[idx['cTotal assets']]/company[idx['cNet sales']] ) <( company[idx['pTotal assets']]/company[idx['pNet Sales']] ) else 0
+			score+=1 if ( company[idx['cTotal assets']]/company[idx['cNet sales']] ) <( company[idx['pTotal assets']]/company[idx['pNet sales']] ) else 0
 		except:
 			score+=1 if ( company[idx['cTotal assets']]/company[idx['cIncome from financial services']] ) <( company[idx['pTotal assets']]/company[idx['pIncome from financial services']] ) else 0
 		f_score.append(score)
@@ -71,21 +67,24 @@ for company in companies_for_removal:
 data = transpose(data)
 data.append(f_score)
 data = transpose(data)
-print len(data)
 # Sorted by BM
 bmsorted_data = sorted(data, key = lambda x: x[idx['cPB on NSE']])
 # Take top 50%
 highbm_data =[i for j,i in enumerate(bmsorted_data) if j in range(0, len(bmsorted_data)/2)]
 # Sorted by f_score
 fsorted_data = sorted(highbm_data, key = lambda x: x[-1])
-bottom_decile =[i for j,i in enumerate(fsorted_data) if j in range(0, 1 + len(fsorted_data)/10)]
+bottom_decile =[i for j,i in enumerate(fsorted_data) if j in range(0, len(fsorted_data)/10)]
 top_decile =[i for j,i in enumerate(fsorted_data) if j in range(9*len(fsorted_data)/10, len(fsorted_data))]
 # bottom_decile =[x for x in highbm_data if x[-1] in [1,2] ]
 # top_decile =[x for x in highbm_data if x[-1] in [7,8] ]
-
-print bottom_decile
-# with open("portfolio.txt", "a") as myfile:
-# 	write(sys.) 
+with open(sys.argv[2], "a") as myfile:
+	myfile.write(sys.argv[1][-9:-5]+"\n"+"long on\n" )
+	for company in top_decile:
+		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cPB on NSE']])+"  "+str(company[-1]) + "\n")
+	myfile.write("\nshort on\n" )
+	for company in bottom_decile:
+		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cPB on NSE']])+ "  "+str(company[-1])+"\n")
+	myfile.write("\n\n" )
 	
 returnt = 0
 returnb = 0
@@ -96,5 +95,5 @@ for company in bottom_decile:
 	returnb+= (company[idx['cAdjusted Closing Price ']]-company[idx['fAdjusted Closing Price ']])/company[idx['cAdjusted Closing Price ']]
 returnb = returnb/len(bottom_decile)
 returns = returnt + returnb
-returns =  (returns*10000/15) #+ 7 #bond rate
+returns =  (returns*100) #+ 7 #bond rate
 print returns
